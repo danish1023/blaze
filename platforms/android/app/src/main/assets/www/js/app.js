cb0dd73614ffa4e4eac41aafcaadfc061689c11d
Anurag Mittal <techwork@Anurags-Mac-mini.local>
2019-07-30 16:47:12 +0530

    debug

--- platforms/android/app/src/main/assets/www/js/app.js
+++ platforms/android/app/src/main/assets/www/js/app.js
@@ -2,7 +2,7 @@
 var $$ = Dom7;
 
 // Framework7 App main instance
-var app  = new Framework7({
+var app = new Framework7({
   root: '#app', // App root element
   id: 'io.md.blaze', // App bundle ID
   name: 'Framework7', // App name
@@ -46,33 +46,19 @@ var mainView = app.views.create('.view-main', {
   url: '/'
 });
 
-var options = {
-  "datePrefix": '__DATE:',
-  "fieldValueDelete": "__DELETE",
-  "fieldValueServerTimestamp" : "__SERVERTIMESTAMP",
-  "persist": true,
-  "config" : {}
-};
- 
-if (cordova.platformId === "browser") {
- 
-  options.config = {
-    apiKey: "AIzaSyAzo0k7orQei71dYHG6kSr0hV1ZGR2-yeI",
-    authDomain: "localhost",
-    projectId: "blazefirestore"
-  };
-}
-
-Firestore.initialise(options).then(function(db) {
-  // Add a second document with a generated ID.
-  db.get().collection("users").add({
-      name: "Alan",
-      email: "alan@xyz.com"
-  })
-  .then(function(docRef) {
-      alert("Document written with ID: ", docRef.id);
-  })
-  .catch(function(error) {
-      alert("Error adding document: ", error);
-  });
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyAzo0k7orQei71dYHG6kSr0hV1ZGR2-yeI",
+  authDomain: "localhost",
+  projectId: "blazefirestore"
 });
+
+var db = firebase.firestore();
+
+db.enablePersistence();
+
+// db.collection("users").get().then((querySnapshot) => {
+//   querySnapshot.forEach((doc) => {
+//     console.log(doc.data().name);
+//   });
+// });
